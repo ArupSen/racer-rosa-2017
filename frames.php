38ce86f2fead745330942d3fa5bc727aa96d6fa5
Arup Sen <arupsen48@gmail.com>
2017-10-02 13:24:59 +0100

    Add example paragraph with markup to frames page

--- frames.php
+++ frames.php
@@ -9,7 +9,30 @@ navbar();
 
 // the unique contents will go here up to the footer
 ?>
-
-
+<div class="">
+<h3>MINIMAL AND STYLISH</h3>
+<p>
+  Our bikes are designed and built on demand. The frames are hand crafted by
+  master frame builders in Steel, Aluminium, Carbon Fibre and Titanium. <span>>>MORE</span>
+  We finish our bikes with top quality parts to meet detailed personal specifications.
+</p>
+<h3>THE SERVICE</h3>
+<p>
+  Yes, our bikes are beautiful, but the most beautiful thing about them is the way
+  they ride. We move riders onto a different league. Our made to <span>>>MORE</span>
+  measure bike projects are designed by a professional bio-mechanic, so they ride perfectly.More comfort and pleasure while riding means more mileage and better fitness over the years; no injuries, no niggles and best possible performances. We check the progress of our clients with follow-up sessions, advice and assistance.
+</p>
+<h3>HERITAGE AND EXPERTISE</h3>
+<p>
+  Experience is a key factor in frame building. We work with the best Italian artisans
+  who have several generations' experience of building <span>>>MORE</span>
+  performance frames at competitive level. We can design any geometry to meet any need and purpose.
+</p>
+<h3>MADE TO LAST</h3>
+<p>
+  It’s a long term investment. Each bike we design is a completely unique, bespoke
+  project, designed and built to be the companion of a lifetime. >>MORE
+</p>
+</div>
 
 <?php footer(); ?>
