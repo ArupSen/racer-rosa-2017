3d3b03fd644267200220bfc37ced469376d19b88
Arup Sen <arupsen48@gmail.com>
2017-10-09 20:40:30 +0100

    Create MORE / LESS functionality for blurb section

--- js/main.js
+++ js/main.js
@@ -2,4 +2,10 @@
 (function(){
   'use strict';
   // Put your javascripty goodness here as well as jquery
+  $('.blurb span').click(function(){
+      $(this).text(function(i,old){
+          return old=='>>MORE' ?  '>>LESS' : '>>MORE';
+      });
+  });
+
 }());
