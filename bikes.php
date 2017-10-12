f3465cffd2855d93b80e512bcb06dab82bc50c24
Arup Sen <arupsen48@gmail.com>
2017-10-12 12:06:52 +0100

    Add sample content and basic style for bikes page

--- bikes.php
+++ bikes.php
@@ -9,7 +9,47 @@ navbar();
 
 // the unique contents will go here up to the footer
 ?>
-
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-lg-7 col-lg-offset-1">
+      <?php img_tag('bikes2.jpg'); ?>
+    </div>
+    <div class="col-lg-4">
+      <h3>JON</h3>
+    <p>Our bikes are designed and built on demand. The <br>
+    frames are hand crafted by our master frame builders in <br>
+    Steel, Aluminium, Carbon Fibre and Titanium.<br>
+    <span>&gt;&gt;MORE</span></p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-lg-7 col-lg-offset-1">
+      <table class="table table-striped text-uppercase table-condensed">
+      <tr><td>frame</td><td>steel columbus</td></tr>
+      <tr><td>fork</td><td>full carbon</td></tr>
+      <tr><td>groupset</td><td>campagnolo record</td></tr>
+      <tr><td>finish kit</td><td>deda zero 100</td></tr>
+      <tr><td>wheels</td><td>handmade, campagnolo hubs/sapim spokes/ambrosio rims</td></tr>
+      <tr><td>saddle</td><td>selle italia slr flow</td></tr>
+      <tr><td>tyres</td><td>vittoria corsa g+</td></tr>
+      </table>
+    </div>
+    <div class="col-lg-4">
+    <p>Our bikes are designed and built on demand. The <br>
+    frames are hand crafted by our master frame builders in <br>
+    Steel, Aluminium, Carbon Fibre and Titanium.<br>
+    <span>&gt;&gt;MORE</span></p>
+    </div>
+<div class="row">
+  <div class="col-lg-7 col-lg-offset-1">
+    <?php img_tag('bikes3.jpg'); ?>
+  </div>
+  <div class="col-lg-4">
+    <?php img_tag('bikes4.jpg'); ?>
+  </div>
+</div>
+  </div>
+</div>
 
 
 <?php footer(); ?>
