83b56a03fe4757be15bb4e68e8d48fd7cf1dcd3b
Arup Sen <arupsen48@gmail.com>
2017-10-13 21:33:08 +0100

    Add dd-formmailer script to site

--- contact.php
+++ contact.php
@@ -9,6 +9,26 @@ navbar();
 
 // the unique contents will go here up to the footer
 ?>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-lg-3 col-md-6 col-sm-12">
+      <?php
+      img_tag('example_pic.jpg');
+      ?>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-12">
+      <?php
+      img_tag('rr_shop.jpg');
+      ?>
+    </div>
+    <div class="clearfix visible-sm-block"></div>
+    <div class="col-lg-5 col-md-6 col-sm-8 blurb">
+<?php
+include_once(INCPATH.'dd-formmailer.php');
+?>
+    </div>
+  </div> <!-- end of .row -->
+</div> <!-- end of .container-fluid -->
 
 
 
