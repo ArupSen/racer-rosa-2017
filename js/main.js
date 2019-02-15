3e820ae363a9f8d8923b35702b583ded5490c498
Arup Sen <arupsen48@gmail.com>
2019-02-15 14:58:57 +0000

    Set up video square to load random YouTube video and also update video title

--- js/main.js
+++ js/main.js
@@ -7,10 +7,18 @@
           return old=='&gt;&gt;MORE' ?  '<i class="sign">></i>LESS' : '&gt;&gt;MORE';
       });
   });
+  // keep height of 'blurb' div same as adjacent images
   $('.blurb span').click(function() {
     var picHeight = $('.pic img')[0].height;
     document.getElementsByClassName('blurb')[0].style.maxHeight = picHeight + 'px';
     document.getElementsByClassName('blurb')[0].style.overflowY = 'scroll';
   });
+  // choose a random YouTube video for the video square
+  var vidList = ['X19ovl-PiYk','kfV2hxl0iI8','ScPIwWyM-cs','euLwtcYDZ58','xNnQlPUhVRc','qbLkDdttQs0'];
+  var vidTitle = ['fitting', 'why made to measure', 'why a steel bike', 'why do I get sore knees', 'why does my saddle hurt', 'why do I get numb hands'];
+  var youTubeURL = "https://www.youtube.com/embed/";
+  var randomNumber = Math.floor(Math.random() * vidList.length);
+  $('iframe')[0].src = youTubeURL + vidList[randomNumber];
+  $('.session-date p').eq(0).text(vidTitle[randomNumber]);
 
 }());
